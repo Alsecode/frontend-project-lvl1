c019456255786e255701ce251a32fa1157f7a54e
Art <fil.flip@mail.ru>
2022-07-18 01:21:49 +0300

    added brain-prime game

---
+++ src/primeGame.js
@@ -0,0 +1,37 @@
+import { getRandomNumber } from './getRandomNumber.js';
+import {
+  greetUser, questionUser, checkUserAnswer, congratulateUser,
+} from './index.js';
+
+const isPrime = (number) => {
+  if (number < 2) {
+    return false;
+  }
+  let noDivisor = true;
+  for (let i = 2; i <= number / 2; i += 1) {
+    if (number % i === 0) {
+      noDivisor = false;
+    }
+  }
+
+  return noDivisor;
+};
+
+export const primeGame = () => {
+  const name = greetUser('Answer "yes" if given number is prime. Otherwise answer "no".');
+  for (let i = 0; i < 3; i += 1) {
+    const question = getRandomNumber();
+    const answer = questionUser(question);
+    let trueAnswer;
+    if (isPrime(question) === true) {
+      trueAnswer = 'yes';
+    } else {
+      trueAnswer = 'no';
+    }
+    const answerIsCorrect = checkUserAnswer(answer, trueAnswer, name);
+    if (answerIsCorrect === false) {
+      return;
+    }
+  }
+  congratulateUser(name);
+};
